508a9e17ee27f270d0bbc39284e900536fbd1fd5
@Desaborokoroko <kgatontle7@gmail.com>
2026-06-17 09:20:57 +0200

    update static

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-local-dev-only-change-me')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True #os.getenv('DEBUG' 'False') == 'True'
+DEBUG = os.getenv('DEBUG' 'False') == 'True'
 
 
 
