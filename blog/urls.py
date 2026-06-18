d28a864c8cc63891d392c6b9f93df9e793670b5b
@Desaborokoroko <kgatontle7@gmail.com>
2026-06-18 09:34:02 +0200

    update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = []
+
+urlpatterns = [path("",views.home,name='home'),
+    path('category/<slug:category_slug>/',views.category_posts,name='category_posts'),
+    path('post/<int:post_id>/',views.post_detail,name='post_detail'),]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
-    urlpatterns = [path("",views.home,name='home'),path('category/<slug:category_slug>/',views.category_posts,name='category_posts'),
-            path('post/<int:post_id>/',views.post_detail,name='post_detail'),]
+    
