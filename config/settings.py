7736b4abc064f220c59b886deede29223ac00c9f
@Desaborokoroko <kgatontle7@gmail.com>
2026-06-19 22:10:08 +0200

    fix media storage and Cloudinary

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,17 @@ CLOUDINARY_STORAGE = {
     'API_KEY':os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET':os.getenv('CLOUDINARY_API_SECRET'),
     }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGEs = { 
+            "default": {
+                "BACKEND":
+                    "cloudinary_storage.storage.MediaCloudinaryStorage",
+            },
+            "staticfiles": {
+                "BACKEND":
+                    "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            }
+    
+}
 
 #upload media
 MEDIA_URL = '/media/'
