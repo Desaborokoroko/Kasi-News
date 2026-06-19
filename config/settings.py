dfcff5ddefdad85d9ef2918b93960652da3ba002
@Desaborokoroko <kgatontle7@gmail.com>
2026-06-19 15:02:15 +0200

    fix Css

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-local-dev-only-change-me')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG' 'False') == 'True'
+DEBUG = os.getenv('DEBUG' , 'False') == 'True'
 
 
 
