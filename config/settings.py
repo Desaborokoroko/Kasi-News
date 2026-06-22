742bf744c53b79a810e173d120c7b7357e5a9a63
@Desaborokoroko <kgatontle7@gmail.com>
2026-06-22 15:40:36 +0200

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,7 @@ STORAGES = {
     },
     "staticfiles": {
         "BACKEND":
-            "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
