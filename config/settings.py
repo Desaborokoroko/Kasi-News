b43c3f88a51b6ca2a0496c452396fad1ec6822b8
@Desaborokoroko <kgatontle7@gmail.com>
2026-06-23 16:25:02 +0200

    fix Value Erro

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,7 @@ STORAGES = {
     },
     "staticfiles": {
         "BACKEND":
-            "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
