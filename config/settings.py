72ec18181d04fb58d0111f8540bb2805dfd8493c
@Desaborokoroko <kgatontle7@gmail.com>
2026-07-06 15:27:32 +0200

    fix CSS and staticfiles

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
 STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.FileSystemFinder',
