e8a3048da574af668a46d1fcc321df48f90c231c
@Desaborokoroko <kgatontle7@gmail.com>
2026-08-29 23:55:05 +0200

    reset to clean code and fix top story cover

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-local-dev-only-change-me')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG' , 'False') == 'True'
+DEBUG = True#os.getenv('DEBUG' , 'False') == 'True'
 
 
 
